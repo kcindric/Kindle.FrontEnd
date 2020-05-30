01c9b104bf7ea062fec1afbe9f648af2d49a19fc
Ivica Batinic <ivica.batinic@infinum.hr>
2020-05-30 13:41:14 +0200

    Add forgot password page

---
+++ src/pages/forgot-password.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, Button, Heading, Field, Styled } from 'theme-ui';
+import { useForm } from 'react-hook-form';
+
+import { AuthLayout } from '../components/layouts/AuthLayout';
+
+export default function Login() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
+  return (
+    <AuthLayout>
+      <Card sx={{ boxShadow: 'lg', p: 3, minWidth: '400px' }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Heading as={Styled.h2} sx={{ textAlign: 'center', mb: 3 }}>
+            Forgot password
+          </Heading>
+          <Field label="Email" name="email" placeholder="Enter email" ref={register} mb={3} />
+          <Button type="submit" sx={{ width: '100%' }}>
+            Submit
+          </Button>
+        </form>
+      </Card>
+    </AuthLayout>
+  );
+}
