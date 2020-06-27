27eb416ac70efb9246522e3680a2395880a87c10
Ivica Batinic <ivica.batinic@infinum.hr>
2020-06-27 12:28:26 +0200

    Try to fix https api error in chrome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'theme-ui';
 import { SWRConfig } from 'swr';
-import { fetcher } from '../libs/fetcher';
+import Head from 'next/head';
 
+import { fetcher } from '../libs/fetcher';
 import theme from '../styles/theme';
 import { GlobalStyles } from '../styles/global';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <GlobalStyles />
       <SWRConfig
         value={{
