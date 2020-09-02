5e4057bb7dc25fa2846a73dc63a00d7ea93130eb
Ivica Batinic <ivica.batinic@infinum.hr>
2020-09-02 18:42:20 +0200

    Finish api proxy

---
+++ src/pages/api/v1/[[...param]].tsx
@@ -0,0 +1,30 @@
+import nc from 'next-connect';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const handler = nc();
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+handler.use(
+  // @ts-ignore
+  createProxyMiddleware({
+    target: 'http://161.35.75.249/api/',
+    pathRewrite: {
+      [`^${process.env.NEXT_PUBLIC_API_ENDPOINT}`]: '',
+    },
+    changeOrigin: true,
+    cookieDomainRewrite: '',
+    onProxyRes: (proxyRes, req, res) => {
+      if (proxyRes.headers['set-cookie']) {
+        const cookies = proxyRes.headers['set-cookie'].map((cookie) => cookie.replace(/; samesite=none/gi, ''));
+        proxyRes.headers['set-cookie'] = cookies;
+      }
+    },
+  }),
+);
+
+export default handler;
