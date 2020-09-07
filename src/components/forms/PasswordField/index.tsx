cf66c5e18753f30affeb150b2e61f34ff38dc0fc
Ivica Batinic <ivica.batinic@infinum.hr>
2020-09-08 00:14:59 +0200

    Fix build error

--- src/components/forms/PasswordField/index.tsx
+++ src/components/forms/PasswordField/index.tsx
@@ -9,7 +9,7 @@ export const PasswordInput = forwardRef<any, InputProps>(function PasswordInput(
 
   return (
     <InputGroup size="md">
-      <Input ref={ref} pr="4.5rem" {...props} type={show ? 'text' : 'password'} type={show ? 'text' : 'password'} />
+      <Input ref={ref} pr="4.5rem" {...props} type={show ? 'text' : 'password'} />
       <InputRightElement width="4.5rem" pr="0">
         <Button variant="ghost" type="button" h="1.75rem" size="sm" onClick={handleClick}>
           {show ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
